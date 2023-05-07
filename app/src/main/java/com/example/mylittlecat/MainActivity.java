bb14277aba11563b1da4962c0adcd3cc40097195
kingulin <kingulin2005@gmail.com>
2023-05-07 19:26:46 +0200

    cat isClean

--- app/src/main/java/com/example/mylittlecat/MainActivity.java
+++ app/src/main/java/com/example/mylittlecat/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     public ImageView catMood;
+    public int catMoodNum = 100;
     public boolean IsCatClean = true;
     public int CatClean = 40;
 
@@ -23,18 +24,34 @@ public class MainActivity extends AppCompatActivity {
         catMood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(catMoodNum > 50){
              if(IsCatClean == true){
              catMood.setImageResource(R.drawable.cat_tap);
             }else{
 
                  catMood.setImageResource(R.drawable.cat_happy_dirty_tap);
              }
-            }
+            }}
         });
-        if(CatClean <= 50){
-            IsCatClean = false;
-            catMood.setImageResource(R.drawable.cat_happy_dirty);
-        }
 
-    }
-}
+                if(CatClean > 50 && catMoodNum > 50) {
+                    IsCatClean = true;
+                    catMood.setImageResource(R.drawable.cat);
+                }
+                else if(CatClean > 50 && catMoodNum <= 50) {
+                    IsCatClean = true;
+                    catMood.setImageResource(R.drawable.cat_sad);
+                      }
+                else if(CatClean <= 50 && catMoodNum > 50) {
+                    IsCatClean = false;
+                    catMood.setImageResource(R.drawable.cat_happy_dirty);
+                }else if(CatClean <= 50 && catMoodNum <= 50){
+
+                    IsCatClean = false;
+                    catMood.setImageResource(R.drawable.cat_sad_dirty);
+                }
+
+
+
+            }
+        }
