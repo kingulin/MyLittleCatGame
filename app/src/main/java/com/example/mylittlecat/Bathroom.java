8fa92378366bfee5770d91a5b6385abdfac29416
kingulin <kingulin2005@gmail.com>
2023-05-08 23:52:20 +0200

    w końcu coś działa

--- app/src/main/java/com/example/mylittlecat/Bathroom.java
+++ app/src/main/java/com/example/mylittlecat/Bathroom.java
@@ -2,26 +2,27 @@ package com.example.mylittlecat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.os.Handler;
 
-//import java.util.logging.Handler;
-import java.util.logging.LogRecord;
+public class Bathroom extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
-
+    public ImageView Shower;
+    public ImageView Soap;
+    public int CatClean;
+    public ImageView Bubbles;
+    private ImageView ArrL2;
+    private ImageView ArrR2;
+    private boolean IsCatClean;
+    private boolean ISActive = true;
     public ImageView catMood;
     public ImageView Mood;
     public int catMoodNum = 100;
     public ImageView Clean;
-    public boolean IsCatClean = true;
-    public static int CatClean = 100;
     public ImageView Hungry;
     public static int CatHungry = 100;
     public ImageView Sleep;
@@ -29,34 +30,78 @@ public class MainActivity extends AppCompatActivity {
     public int money = 0;
     public String moneyShow;
     public TextView moneyShowFinal;
-    public ImageView Lamp;
-    public static ImageView Dark;
-    public boolean IsSleep = false;
-    public boolean ISActive = true;
-    public ImageView ArrR;
-    public ImageView ArrL;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//set cats mood
-
-
-
-
+        setContentView(R.layout.activity_bathroom);
+//clean cat
+        Shower = findViewById(R.id.shower);
+        Soap = findViewById(R.id.soap);
+        Bubbles = findViewById(R.id.bubbles);
+        ArrL2 = findViewById(R.id.arrleft2);
         catMood = findViewById(R.id.catMood);
         Mood = findViewById(R.id.mood);
         Clean = findViewById(R.id.clean);
         Hungry = findViewById(R.id.hungry);
         Sleep = findViewById(R.id.sleep);
+        ArrR2 = findViewById(R.id.arrright2);
 
         Intent takeintent = getIntent();
         IsCatClean = takeintent.getBooleanExtra("IsCatClean_key", true);
-        CatClean = takeintent.getIntExtra("CatClean_key", 100);
-        catMoodNum = takeintent.getIntExtra("catMoodNum_key", 100);
-        CatHungry = takeintent.getIntExtra("CatHungry_key", 100);
-        CatSleep = takeintent.getIntExtra("CatSleep_key", 100);
+        CatClean = takeintent.getIntExtra("CatClean_key", 0);
+        catMoodNum = takeintent.getIntExtra("catMoodNum_key", 0);
+        CatHungry = takeintent.getIntExtra("CatHungry_key", 0);
+        CatSleep = takeintent.getIntExtra("CatSleep_key", 0);
         money = takeintent.getIntExtra("money_key", 0);
+        Soap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bubbles.setAlpha((float) 1);
+            }
+        });
+        Shower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bubbles.setAlpha((float) 0);
+                CatClean = 100;
+            }
+        });
+        //        //switch layout
+//
+
+        ArrR2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Bathroom.this, Kitchen.class);
+                intent.putExtra("catMoodNum_key", catMoodNum);
+                intent.putExtra("IsCatClean_key", IsCatClean);
+                intent.putExtra("CatClean_key", CatClean);
+                intent.putExtra("CatHungry_key", CatHungry);
+                intent.putExtra("CatSleep_key", CatSleep);
+                intent.putExtra("money_key", money);
+                startActivity(intent);
+            }
+        });
+           ArrL2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  Intent intent = new Intent(Bathroom.this, MainActivity.class);
+                intent.putExtra("catMoodNum_key", catMoodNum);
+                intent.putExtra("IsCatClean_key", IsCatClean);
+                intent.putExtra("CatClean_key", CatClean);
+                intent.putExtra("CatHungry_key", CatHungry);
+                intent.putExtra("CatSleep_key", CatSleep);
+                intent.putExtra("money_key", money);
+                startActivity(intent);
+            }
+        });
+
+
+
+//set cats mood
+
         catMood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,16 +117,15 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }}
         });
-
-        //set stats change
+//
         if(ISActive){
-        final Handler handler = new Handler();
+//        //set stats change
+        final android.os.Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (CatHungry > 0) CatHungry -= 1.5;
-                if (CatSleep > 0 && !IsSleep) CatSleep -= 2;
-                else if(CatSleep <= 100 && IsSleep) CatSleep +=10;
+                if (CatSleep > 0 ) CatSleep -= 2;
                 if (CatClean > 0) CatClean -= 5;
                 if (catMoodNum > 0) {
                     catMoodNum -= 2;
@@ -96,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                if(!IsSleep) {
                     if (CatClean > 50 && catMoodNum > 50) {
                         IsCatClean = true;
                         catMood.setImageResource(R.drawable.cat);
@@ -110,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
 
                         IsCatClean = false;
                         catMood.setImageResource(R.drawable.cat_sad_dirty);
-                    }}
+                    }
                 //ico changes
                 if (CatClean >= 50) {
                     Clean.setImageResource(R.drawable.clean_full);
@@ -156,8 +199,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         },2*1000);}
-
-
         // sets money
 
         moneyShow = getString(R.string.money_status);
@@ -166,54 +207,7 @@ public class MainActivity extends AppCompatActivity {
 
         moneyShowFinal = findViewById(R.id.money_status_id);
         moneyShowFinal.setText(moneyShow);
-
-        //lamp
-        Lamp = findViewById(R.id.lamp);
-        Dark = findViewById(R.id.dark);
-
-        Lamp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(!IsSleep){
-                    IsSleep = true;
-                    catMood.setImageResource(R.drawable.cat_sleep);
-                    Dark.setAlpha((float) 0.6);
-                }else
-                {
-                    IsSleep = false;
-                    Dark.setAlpha((float)0);
-                }
-            }
-        });
-
-        //switch layout
-
-        ArrR = findViewById(R.id.arrright);
-        ArrL = findViewById(R.id.arrleft);
-
-        ArrR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // setContentView(R.layout.activity_bathroom);
-                Intent intent = new Intent(MainActivity.this, Bathroom.class);
-                intent.putExtra("catMoodNum_key", catMoodNum);
-                intent.putExtra("IsCatClean_key", IsCatClean);
-                intent.putExtra("CatClean_key", CatClean);
-                intent.putExtra("CatHungry_key", CatHungry);
-                intent.putExtra("CatSleep_key", CatSleep);
-                intent.putExtra("money_key", money);
-                startActivity(intent);
-            }
-        });
-
-//        ArrL.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                setContentView(R.layout.);
-//            }
-//        });
-    }
+        }
     @Override
     protected void onResume() {
         super.onResume();
@@ -225,4 +219,6 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         ISActive = false;
     }
+
+
 }
---
+++ app/src/main/java/com/example/mylittlecat/Kitchen.java
@@ -0,0 +1,23 @@
+package com.example.mylittlecat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Kitchen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen);
+
+
+
+
+    }
+}
